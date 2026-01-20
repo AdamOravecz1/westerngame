34618b2b10ce8abd56eaa2a6d3b022ebea741b93
AdamOravecz1 <oravecz.adam2004@gmail.com>
2026-01-20 19:56:24 +0100

    ragdoll

--- Scripts/basic_enemy.gd
+++ Scripts/basic_enemy.gd
@@ -1,9 +1,13 @@
 extends CharacterBody3D
 
-@onready var enemy_anim: AnimationPlayer = $BasicEnemy2.get_node("AnimationPlayer")
+@onready var enemy_anim: AnimationPlayer = $BasicEnemy3.get_node("AnimationPlayer")
 
 @export var speed: float = 2.0
 @export var rotation_speed: float = 6.0
+
+@onready var skeleton := $BasicEnemy3/Armature/Skeleton3D/PhysicalBoneSimulator3D
+
+var is_ragdoll := false
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
@@ -33,3 +37,41 @@ func _physics_process(delta):
 
 func hit(hitbox_type: String):
 	print("Hit:", hitbox_type)
+	if is_ragdoll:
+		apply_ragdoll_impulse(hitbox_type)
+	else:
+		die()
+	
+func die():
+	if is_ragdoll:
+		return
+
+	is_ragdoll = true
+
+	# Stop animations
+	enemy_anim.stop()
+
+	# Enable physics on bones
+	skeleton.physical_bones_start_simulation()
+
+	# Disable character movement
+	set_physics_process(false)
+	
+func apply_ragdoll_impulse(hitbox_type: String):
+	var impulse_dir = -global_basis.z
+	impulse_dir.y += 0.8
+	impulse_dir = impulse_dir.normalized()
+
+	var impulse_strength := 6.0
+
+	# Try to hit a specific bone (e.g. head), fallback to all
+	var bone_name := hitbox_type.capitalize()
+
+	var bone := skeleton.get_node_or_null(bone_name)
+	if bone and bone is PhysicalBone3D:
+		bone.apply_impulse(impulse_dir * impulse_strength)
+	else:
+		# Fallback: apply impulse to all bones
+		for child in skeleton.get_children():
+			if child is PhysicalBone3D:
+				child.apply_impulse(impulse_dir * impulse_strength * 0.3)
