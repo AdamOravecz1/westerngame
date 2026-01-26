da568726941220915f3d25cdc1b436cb95d61617
AdamOravecz1 <oravecz.adam2004@gmail.com>
2026-01-26 21:16:28 +0100

    enemy animation tree and logic

--- Scripts/basic_enemy.gd
+++ Scripts/basic_enemy.gd
@@ -2,37 +2,78 @@ extends CharacterBody3D
 
 @onready var enemy_anim: AnimationPlayer = $BasicConnectedDude.get_node("AnimationPlayer")
 
+
 @export var speed: float = 2.0
 @export var rotation_speed: float = 6.0
 
 @onready var skeleton := $BasicConnectedDude/Armature/Skeleton3D/PhysicalBoneSimulator3D
+@onready var model: Node3D = $BasicConnectedDude
+
 
 var is_ragdoll := false
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
+@onready var run_ray: RayCast3D = $RunRay
+@onready var aim_ray: RayCast3D = $AimRay
+
+
+
 func _physics_process(delta):
 	if player == null:
 		return
 
-	var direction = player.global_position - global_position
-	direction.y = 0
+	# Rotate ray to face player (Y axis only)
+	var ray_pos = run_ray.global_position
+	var player_pos = player.global_position
 
-	if direction.length() < 0.1:
+	var dir = player_pos - ray_pos
+	dir.y = 0
+
+	var target_y = atan2(dir.x, dir.z)
+	run_ray.global_rotation.y = target_y
+	aim_ray.global_rotation.y = target_y
+	
+	# === Check if player is visible ===
+	var sees_player := run_ray.is_colliding() and run_ray.get_collider() == player
+
+	if sees_player:
+		
+		# Movement direction
+		var direction = player.global_position - global_position
+		direction.y = 0
+
+		if direction.length() < 0.1:
+			velocity = Vector3.ZERO
+			move_and_slide()
+			return
+
+		direction = direction.normalized()
+
+		# Move the physics body forward
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+
+		# Rotate only the model to face player
+		var target_rot = Vector3(0, atan2(direction.x, direction.z), 0)
+		model.rotation = model.rotation.slerp(target_rot, rotation_speed * delta)
+		
+		if aim_ray.is_colliding():
+			if speed == 2:
+				$BasicConnectedDude/AnimationTree.set("parameters/TimeSeek/seek_request", 0)
+			
+			$BasicConnectedDude/AnimationTree.set("parameters/Blend3/blend_amount", -1)
+			
+			speed = 1
+		else:
+			$BasicConnectedDude/AnimationTree.set("parameters/Blend3/blend_amount", 1)
+			speed = 2
+	else:
+		$BasicConnectedDude/AnimationTree.set("parameters/Blend3/blend_amount", 0)
 		velocity = Vector3.ZERO
-		move_and_slide()
-		return
-
-	direction = direction.normalized()
-
-	var target_basis = Basis.looking_at(-direction, Vector3.UP)
-	global_basis = global_basis.slerp(target_basis, rotation_speed * delta)
-
-	var forward = global_basis.z
-	velocity.x = forward.x * speed
-	velocity.z = forward.z * speed
 
 	move_and_slide()
+	
 
 
 func hit(hitbox_type: String):
