c61f9f56e13265dfcbc2e8c319473f8506464281
AdamOravecz1 <oravecz.adam2004@gmail.com>
2026-08-16 17:22:21 +0200

    fade outs

--- Scripts/barrier_broken.gd
+++ Scripts/barrier_broken.gd
@@ -13,6 +13,26 @@ func _ready() -> void:
 
 	nav_mesh.bake_navigation_mesh()
 	await nav_mesh.bake_finished
-	await  get_tree().create_timer(3, false).timeout
+	await  get_tree().create_timer(5, false).timeout
 	
+	fade_out()
+
+func fade_out():
+	var meshes = find_children("*", "MeshInstance3D", true, false)
+	var tween = create_tween()
+
+	for mesh in meshes:
+		var material = mesh.get_active_material(0)
+
+		if material:
+			material = material.duplicate()
+		else:
+			material = StandardMaterial3D.new()
+
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_HASH
+		mesh.material_override = material
+
+		tween.parallel().tween_property(material, "albedo_color:a", 0.0, 1.5)
+
+	await tween.finished
 	queue_free()
--- Scripts/basic_enemy.gd
+++ Scripts/basic_enemy.gd
@@ -81,6 +81,26 @@ func die(from_position: Vector3 = global_position, strength: float = 0.0, l_damp
 	
 
 	await get_tree().create_timer(5, false).timeout
+	fade_out()
+	
+func fade_out():
+	var meshes = find_children("*", "MeshInstance3D", true, false)
+	var tween = create_tween()
+
+	for mesh in meshes:
+		var material = mesh.get_active_material(0)
+
+		if material:
+			material = material.duplicate()
+		else:
+			material = StandardMaterial3D.new()
+
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_HASH
+		mesh.material_override = material
+
+		tween.parallel().tween_property(material, "albedo_color:a", 0.0, 1.5)
+
+	await tween.finished
 	queue_free()
 	
 func follow_path(where, delta):
