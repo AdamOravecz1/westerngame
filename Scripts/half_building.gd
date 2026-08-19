034bc58f69b0452021de3ce0d5c6e07d34e928ce
AdamOravecz1 <oravecz.adam2004@gmail.com>
2026-08-19 18:35:47 +0200

    building explanations

--- Scripts/half_building.gd
+++ Scripts/half_building.gd
@@ -5,31 +5,46 @@ extends Node3D
 @onready var nav_mesh = get_tree().get_first_node_in_group("NavigationRegion")
 @export var building_scene = preload("res://Scenes/barracks.tscn")
 
+@onready var hover_rect: ColorRect = $CanvasLayer/HoverRect
+
+var upgrade_text = "Unlocks access to the sniper tower on the top\nCost: 20"
+
 func _ready() -> void:
 	print(name)
 	if name == "HalfBarracks":
+		upgrade_text = "Produces more soldiers\nCost: 20"
 		await get_tree().process_frame
 		main.barracks += 1
 		main.friend_wave_point = $FriendWavePoint.global_position
 		main.friend_counter = 1
 	if name == "HalfSaloon":
+		upgrade_text = "Restores more health\nCost: 20"
 		main.saloon += 1
 	if name == "HalfStore":
+		upgrade_text = "Gives more ammo and dynamite\nCost: 20"
 		main.store += 1
 	if name == "HalfMine":
+		upgrade_text = "Produces more money\nCost: 20"
 		main.mine += 1
-	await get_tree().create_timer(2.0, false).timeout
-	if nav_mesh.is_baking():
-		await nav_mesh.bake_finished
 	if name == "HalfBlackSmith":
+		upgrade_text = "Unlocks sniper\nCost: 20"
 		await get_tree().process_frame
 		player.unlock_shotgun()
 	if name == "HalfCarpenter":
+		upgrade_text = "Unlucks exploading barrels and allows more covers\nCost: 20"
 		main.carpenter = self
+	await get_tree().create_timer(2.0, false).timeout
+	if nav_mesh.is_baking():
+		await nav_mesh.bake_finished
+
 
 	nav_mesh.bake_navigation_mesh()
 	await nav_mesh.bake_finished
 	await get_tree().create_timer(3, false).timeout
+	
+func _process(_delta: float) -> void:
+	if hover_rect and hover_rect.visible:
+		hover_rect.global_position = get_viewport().get_mouse_position() + Vector2(10, 10)
 
 func menu():
 	$CanvasLayer.visible = !$CanvasLayer.visible
@@ -106,3 +121,13 @@ func _on_next_wave_pressed() -> void:
 
 func update_barriers(num):
 	$CanvasLayer/BarrierCounter.text = str(num) + "/4"
+
+
+func _on_upgrade_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$CanvasLayer/HoverRect/RichTextLabel.text = upgrade_text
+
+
+func _on_upgrade_mouse_exited() -> void:
+	hover_rect.visible = false
--- Scripts/shop_menu.gd
+++ Scripts/shop_menu.gd
@@ -2,7 +2,19 @@ extends Control
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var shop_menu = player.get_node("CanvasLayer/ShopMenu")
+@onready var hover_rect: ColorRect = $HoverRect
 var foundation = null
+
+const BLACKSMITHCOST := 10
+const STORECOST := 10
+const CARPENTERCOST := 10
+const BARRACKSCOST := 10
+const MINECOST := 10
+const SALOONCOST := 10
+
+func _process(_delta: float) -> void:
+	if hover_rect and hover_rect.visible:
+		hover_rect.global_position = get_viewport().get_mouse_position() + Vector2(10, 10)
 
 func shop(found):
 	foundation = found
@@ -17,35 +29,95 @@ func shop(found):
 
 
 func _on_black_smith_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= BLACKSMITHCOST:
 		foundation.build("res://Scenes/half_black_smith.tscn")
 		$VBoxContainer/BlackSmith.queue_free()
 
 
 func _on_store_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= STORECOST:
 		foundation.build("res://Scenes/half_store.tscn")
 		$VBoxContainer/Store.queue_free()
 
 func _on_carpenter_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= CARPENTERCOST:
 		foundation.build("res://Scenes/half_carpenter.tscn")
 		$VBoxContainer/Carpenter.queue_free()
 
 
 func _on_barracks_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= BARRACKSCOST:
 		foundation.build("res://Scenes/half_barracks.tscn")
 		$VBoxContainer/Barracks.queue_free()
 
 
 func _on_mine_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= MINECOST:
 		foundation.build("res://Scenes/half_mine.tscn")
 		$VBoxContainer/Mine.queue_free()
 
 
 func _on_saloon_pressed() -> void:
-	if player.money >= 10:
+	if player.money >= SALOONCOST:
 		foundation.build("res://Scenes/half_saloon.tscn")
 		$VBoxContainer/Saloon.queue_free()
+
+
+func _on_black_smith_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Unlocks shotgun\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_black_smith_mouse_exited() -> void:
+	hover_rect.visible = false
+
+
+func _on_store_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Gives ammo after every wave\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_store_mouse_exited() -> void:
+	hover_rect.visible = false
+
+
+func _on_carpenter_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Allows the building of covers\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_carpenter_mouse_exited() -> void:
+	hover_rect.visible = false
+
+
+func _on_barracks_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Produces soldiers that fight on your side\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_barracks_mouse_exited() -> void:
+	hover_rect.visible = false
+
+
+func _on_mine_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Extra money after each wave\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_mine_mouse_exited() -> void:
+	hover_rect.visible = false
+
+
+func _on_saloon_mouse_entered() -> void:
+	if not hover_rect.visible:
+		hover_rect.visible = true
+		$HoverRect/RichTextLabel.text = "Refils some health after each wave\n Cost: " + str(BLACKSMITHCOST)
+
+
+func _on_saloon_mouse_exited() -> void:
+	hover_rect.visible = false
